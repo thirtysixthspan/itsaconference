238727b30ab7216fa778c3c2af9a75e4a6efd9d6
Derrick Parkhurst <derrickparkhurst@derrick-parkhursts-macbook-pro.local>
2010-01-27 06:19:34 -0600

    haml and 960 added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,21 @@
-# Filters added to this controller apply to all controllers in the application.
-# Likewise, all the methods added will be available for all controllers.
-
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
-  # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
+  filter_parameter_logging :password
+  
+  before_filter :add_stylesheets
+
+  def initialize
+    @stylesheets = []
+    @website_title = "Red Dirt Duby Conference"
+  end
+
+  def add_stylesheets
+    ["#{controller_name}/_controller", "#{controller_name}/#{action_name}"].each do |stylesheet|
+      @stylesheets << stylesheet if File.exists? "#{RAILS_ROOT}/public/stylesheets/#{stylesheet}.css"
+    end
+  end
+  
+  
 end
