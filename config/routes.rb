cd70649ac345b83b62aea301cff90287d1d03f4a
Derrick Parkhurst <derrickparkhurst@derrick-parkhursts-macbook-pro.local>
2010-01-30 04:23:39 -0600

    integrated design

--- config/routes.rb
+++ config/routes.rb
@@ -40,4 +40,9 @@ ActionController::Routing::Routes.draw do |map|
   # consider removing or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+  
+  map.root :controller=>"front", :action=>"index"
+  map.connect 'program', :controller=>"front", :action=>"program"
+  map.connect 'contact', :controller=>"front", :action=>"contact"
+
 end
