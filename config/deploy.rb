a744c84aaaada19a006959e0859c41028e24460e
Derrick Parkhurst <derrickparkhurst@derrick-parkhursts-macbook-pro.local>
2010-01-30 17:15:10 -0600

    cap deploy setup

---
+++ config/deploy.rb
@@ -0,0 +1,13 @@
+require "config/database"
+
+set :application, "itsaconference"
+set :repository,  "git@github.com:thirtysixthspan/itsaconference.git"
+
+set :template_dir, "config/"
+set :scm, :git
+set :deploy_to, "/srv/www/reddirtrubyconf.com"
+set :user, "deployer"
+
+server "reddirtrubyconf.com", :app, :web, :db, :primary => true
+
+ssh_options[:forward_agent] = true
