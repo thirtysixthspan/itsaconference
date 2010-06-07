a16e52b49677c5925899b1c085326f5819735154
Derrick Parkhurst <derrickparkhurst@derrick-parkhursts-macbook-pro.local>
2010-06-07 02:56:28 -0500

    update

--- lib/stream.rb
+++ lib/stream.rb
@@ -5,17 +5,18 @@ module ActionController
 
       def stream_file(path, options = {})
         raise MissingFile, "Cannot read file #{path}" unless File.file?(path) and File.readable?(path)
-
         options[:start]    ||= "0"
-        options[:length]   ||= File.size(path) - options[:start].to_i
+        options[:length]   ||= File.size(path)
+        options[:length] -= options[:start].to_i
         options[:filename] ||= File.basename(path) unless options[:url_based_filename]
-        send_file_headers! options if options[:start]=="0"
+        send_file_headers! options
 
         render :status => options[:status], :text => Proc.new { |response, output|
           logger.info "Streaming file #{path}" unless logger.nil?
           len = options[:buffer_size] || 4096
           File.open(path, 'rb') do |file|
             file.seek(options[:start].to_i)
+            output.write("FLV\x1\x1\0\0\0\x9\0\0\0\x9")
             while buf = file.read(len)
               output.write(buf)
             end
